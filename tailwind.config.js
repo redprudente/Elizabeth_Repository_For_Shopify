59d16d323e1a7acc66234b6249f5ad1d547293c6
Red Prudente <creative.redprudente@gmail.com>
2024-08-26 17:07:13 +0800

    Added swiper on the product template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './layout/*.liquid',
-    './sections/*.liquid',
-    './snippets/*.liquid',
-    './templates/customers/*.liquid',
-    './templates/*liquid'
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/customers/*.liquid",
+    "./templates/*liquid",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        94: "22rem",
+      },
+    },
   },
   plugins: [],
 };
